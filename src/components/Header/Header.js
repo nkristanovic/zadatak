aafa7afdf652f7ab548d0632dd035878535ef85a
Nikola Kristanović <nikola.kristanovic@gmail.com>
2022-04-08 21:13:01 +0200

    Created header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Header.scss';
+import vectors from '../../assets/images/vectors.png';
+const Header = () => {
+    return (
+        <header className="Header">
+            <div className="Header-Inner">
+                <nav className="Header-Nav">
+                    <ul to="/" className="Header-NavItem" activeclassname="Header-NavItem_active">proizvodi</ul>
+                    <ul to="/" className="Header-NavItem" activeclassname="Header-NavItem_active">usluge</ul>
+                    <ul to="/" className="Header-NavItem" activeclassname="Header-NavItem_active">tečajevi</ul>
+                    <ul to="/" className="Header-NavItem" activeclassname="Header-NavItem_active">o nama</ul>
+                </nav>
+                <nav to="/" className="Header-ButtonContainer">
+                    <img src={vectors} className="Header-Vectors" />
+                    <ul className="Header-Text">cjenik</ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
