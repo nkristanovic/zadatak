b86318f75f62c55fefebdec66bad422315dbef04
Nikola Kristanović <nikola.kristanovic@gmail.com>
2022-04-09 22:56:52 +0200

    Created grid

--- src/components/HeroBottom/HeroBottom.js
+++ src/components/HeroBottom/HeroBottom.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import './HeroBottom.scss';
 import img6 from '../../assets/images/img6.png';
+import Grid from '../Grid/Grid';
 const HeroBottom = () => {
 
     return (
         <div className="HeroBottom">
-            <div className='HeroBottom-Wrapper'>
-                <figure className='HeroBottom-Figure'>
-                    <img src={img6} alt="image6" className="HeroBottom-Image" />
-                </figure>
-                <p className="HeroBottom-Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div className='HeroBottom-Wrapper'>
-                <figure className='HeroBottom-Figure'>
-                    <img src={img6} alt="image6" className="HeroBottom-Image" />
-                </figure>
-                <p className="HeroBottom-Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
-            <div className='HeroBottom-Wrapper'>
-                <figure className='HeroBottom-Figure'>
-                    <img src={img6} alt="image6" className="HeroBottom-Image" />
-                </figure>
-                <p className="HeroBottom-Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </div>
+            <Grid>
+                <div className='HeroBottom-Wrapper'>
+                    <figure className='HeroBottom-Figure'>
+                        <img src={img6} alt="image6" className="HeroBottom-Image" />
+                    </figure>
+                    <p className="HeroBottom-Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div className='HeroBottom-Wrapper'>
+                    <figure className='HeroBottom-Figure'>
+                        <img src={img6} alt="image6" className="HeroBottom-Image" />
+                    </figure>
+                    <p className="HeroBottom-Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+                <div className='HeroBottom-Wrapper'>
+                    <figure className='HeroBottom-Figure'>
+                        <img src={img6} alt="image6" className="HeroBottom-Image" />
+                    </figure>
+                    <p className="HeroBottom-Description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </div>
+            </Grid>
         </div>
     );
 }
